159e5f175cea03321d86010fe34f4f4545839233
mixoni <jmix23@gmail.com>
2018-07-31 13:41:38 +0200

    Updates

---
+++ components/Animation.jsx
@@ -0,0 +1,167 @@
+﻿var React = require("react");
+var ReactDOM = require("react-dom");
+var Circle = require('../components/Circle');
+
+ class Animation extends React.Component {    
+    constructor(props){
+        super(props);
+        this.state = {
+                    showMe: "No",
+                    selectedNumbers : [],
+                    totalSum : 0,
+                    minThreeSum: 0,
+                    maxThreeSum : 0,
+                    minFiveSum : 0,
+                    maxFiveSum : 0,
+                    minNumber : 0,
+                    maxNumber: 0,
+                    even : 0,
+                    odd : 0                   
+        };
+        //this.calculateStatistics  = this.calculateStatistics.bind(this, 'Parameter');
+    }   
+
+    componentDidMount(){
+        let $this = this;
+        setTimeout(function(){ 
+                     $this.setState({ showMe : "Yes" });
+                }, 1000);  
+    }
+ 
+    propTypes: {
+        onComplete: React.PropTypes.func,
+        onTimerExpired:React.PropTypes.func
+    }
+    calculateStatistics = ($value) => {
+       let $this = this;
+       let selNumbers = [...this.state.selectedNumbers];
+       selNumbers.push($value)
+       this.setState({selectedNumbers:[...selNumbers]});
+       console.log('selectedNumbers after push the value' + [...this.state.selectedNumbers])
+       //--- total sum 
+       let total = this.state.selectedNumbers.reduce((a, b) => a + b, 0);
+       this.setState({totalSum:total});
+       
+       //--- sort ascending => min to max
+       let selectedNumbersSorted = [...this.state.selectedNumbers].sort(function(a, b){return a - b});
+
+       //-- min number
+       let minNo = selectedNumbersSorted[0];
+       this.setState({minNumber:selectedNumbersSorted[0]}); 
+   
+       //-- max number
+       let maxNo = selectedNumbersSorted[selectedNumbersSorted.length-1];
+       this.setState({maxNumber:maxNo});  
+   
+
+       //--- sum min 3 numbers
+       if(selectedNumbersSorted.length >= 3 ){
+            let minThreesum = selectedNumbersSorted.slice(0, 3).reduce((a, b) => a + b, 0);
+            $this.setState({minThreeSum : minThreesum});         
+
+           //--- sum max 3 numbers
+           let maxThreesum = selectedNumbersSorted.slice(selectedNumbersSorted.length - 3, selectedNumbersSorted.length).reduce((a, b) => a + b, 0);
+           $this.setState({maxThreeSum : maxThreesum});
+       }
+       
+       if(selectedNumbersSorted.length >= 5){
+           //--- sum min 5 numbers
+           let minFiveSum = selectedNumbersSorted.slice(0, 5).reduce((a, b) => a + b, 0);
+           $this.setState({minFiveSum : minFiveSum});  
+
+           //--- sum max 5 numbers
+           let maxFiveSum = selectedNumbersSorted.slice(selectedNumbersSorted.length - 5, selectedNumbersSorted.length).reduce((a, b) => a + b, 0);
+           $this.setState({maxFiveSum : maxFiveSum});
+       }
+       //--- even : odd
+       this.setState({even : this.even(selectedNumbersSorted)});
+       this.setState({odd : this.odd(selectedNumbersSorted)});
+    
+    } 
+
+    even = (array) => {
+        let counter = 0 ;
+        for (var i = 0; i < array.length; i++) {
+             if(array[i] % 2 === 0)
+                counter++;
+        }
+        return counter;
+    }
+
+    odd = (array) => {
+        let counter = 0 ;
+        for (var i = 0; i < array.length; i++) {
+             if(array[i] % 2 !== 0)
+                counter++;
+        }
+        return counter;
+    }
+
+    render() {
+        return(
+            <div> 
+                <div className="col-md-12">
+				    <div className="col-md-4 put-left">
+					    <div className="row row-2">
+						    <div className="col-md-12">
+                                <Circle list={[0]} className="ball-logo" id="ball-logo"/> 
+                                <Circle list={["G2"]} className="ball-game" id="ball-game-2" />                                                
+                                <Circle list={["G3"]} className="ball-game" id="ball-game-3" />                                                
+                                <Circle list={["G4"]} className="ball-game" id="ball-game-4" />                                                
+                                <Circle list={["G5"]} className="ball-game" id="ball-game-5" />                                            
+						    </div>
+					    </div>
+					    <div className="row row-10">
+						    <div className="col-md-12">
+                                <div className="statistic-result">
+                                    <div>Zbir izvučenih brojeva : </div><div>{ this.state.totalSum }  </div>
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Zbir 3 najmanja broja : </div><div>{ this.state.minThreeSum }</div>  
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Zbir 3 najveća broja : </div><div>{ this.state.maxThreeSum }</div>  
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Zbir 5 najmanja broja : </div><div>{ this.state.minFiveSum }</div>  
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Zbir 5 najvećih brojeva : </div><div>{ this.state.maxFiveSum }</div>
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Najmanji broj : </div><div>{ this.state.minNumber }  </div>
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Najveći broj : </div><div>{ this.state.maxNumber }  </div>
+                                </div>
+                                <div className="statistic-result">
+                                    <div>Par - Nepar : </div><div> { this.state.even } -  { this.state.odd } </div>
+                                </div>
+						    </div>
+					    </div>
+				    </div>
+				    <div className="col-md-8 put-right">
+					    <div className="row row-9">
+						    <div show-me={this.state.showMe} className="col-md-12">
+                                <Circle list={[0]} className="center-circle" id="center" /> 
+                                <Circle list={[0]} className="center-circle-outside" id="center-outside" /> 
+                                <Circle list={this.props.numbers} onComplete = {this.props.onComplete} updateStatistics={(i) => this.calculateStatistics(i)} animation={true} className="ball-circle" id="ball"/> 
+						    </div>
+                            <div className="animation-uninques">
+                                <span>5616846685</span>
+                                <span>09:30</span>
+                            </div>
+					    </div>
+					    <div className="row row-3">
+						    <div className="col-md-12">
+                                    {this.props.numbers.map((object, i) => <Circle list={[0]} dataBadge={i+1} className="ball-numbers" key={i}/>)}                                                                                                                                     
+						    </div>
+					    </div>
+				    </div>
+			    </div>
+            </div> 
+        )        
+    }
+}
+
+module.exports = Animation;
---
+++ components/Circle.jsx
@@ -0,0 +1,304 @@
+﻿var React = require("react");
+var ReactDOM = require("react-dom");
+var LuckyNumber = require('./LuckyNumber');
+
+class Circle extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { className : this.props.className, 
+                       listItems:this.props.list, 
+                       id:this.props.id, 
+                       angle : 0, 
+                       angles:[],
+                       cx:0,
+                       cy:0,
+                       radius: 0,
+                       radisuSat: 0,
+                       deg2rad:0,
+                       dataBadge : this.props.dataBadge                       
+        };
+        this.Loop  = this.Loop.bind(this);
+        this.Animation  = this.Animation.bind(this);
+        this.handleChange  = this.handleChange.bind(this);
+    }
+
+propTypes: {
+        onComplete: React.PropTypes.func,
+        updateStatistics: React.PropTypes.func
+}
+
+componentDidMount(){
+    if(this.state.className == "ball-circle"){
+        this.Animation();
+        let counter = 0;
+        let counterLuckyNumbers = 1;
+        let $interval = 6000;
+        let $ballwithBadgeInterval = 5300;
+        let $ballClockMoveInterval = 4700;
+        let $clockMoveInterval = 5200;
+        let drumInterval = 1000/40; // => 60 fps = 60 pictures/sec
+        let $value = 0;
+        let numbersArray = [...this.state.listItems];
+        let $this = this;
+        let centerElementToUpdate = document.querySelector('.center-circle');
+        
+        let renderTimer = 100;  
+        centerElementToUpdate.style.backgroundImage="url(./Images/Drum/Lotto0" + renderTimer + ".png)"; 
+
+        let startRenderDrumAnimation = setInterval(function(){
+            renderTimer++;
+            centerElementToUpdate.style.backgroundImage="url(./Images/Drum/Lotto0" + renderTimer + ".png)";             
+            console.log('renderTimer: ' + renderTimer);
+            if(renderTimer > 527)
+            {
+              renderTimer = 110;
+              //clearInterval(startRenderDrumAnimation);   
+            }       
+        },drumInterval);
+
+        let exLoop = setInterval(function() {
+            let elementToUpdate = document.querySelector('input.input-ball-circle:last-of-type');
+            let badgeElementToUpdate = document.querySelector('[data-badge="' + counterLuckyNumbers + '"]');
+            //console.log("elementToUpdate: " + elementToUpdate);            
+            if(counter > numbersArray.length - 1 || !elementToUpdate)
+            {
+                clearInterval(exLoop); 
+
+                centerElementToUpdate.innerHTML = "<div class='center-ball-animation-finished-text'>Kraj</div>";         
+                setTimeout(function(){
+                    $this.props.onComplete();
+                }, 2000);               
+                
+            }
+            else{                            
+                $value = numbersArray[counter]; //++counter + $interval/1000;
+                console.log(counterLuckyNumbers);
+                let color = $this.handleBallColor($value);
+                         
+                let $classAnimation = "ball-selected-animation ball-selected-" + color;
+                let $classAnimationBadges = "ball-selected-badges ball-selected-" + color;
+                let $class = "ball-selected ball-selected-" + color;
+
+                centerElementToUpdate.innerHTML = "<div class='center-ball-animation'>" + $value + "</div>";          
+
+                setTimeout(function(){
+                    let currentValue =  $value;
+                    let currentBallClass = $classAnimation;
+                    elementToUpdate.parentElement.innerHTML = "<div class='" + currentBallClass +"'><div class='ball-inner-circle'><div class='ball-circle-number'><span>" + currentValue + "</span></div></div></div>";
+                }, $ballClockMoveInterval);
+
+               setTimeout(function(){ 
+                        let currentValue =  $value;
+                        let currentBallClass = $classAnimationBadges;
+                        badgeElementToUpdate.innerHTML = "<div class='" + currentBallClass +"'><div class='ball-inner-circle'><div class='ball-circle-number'><span>" + currentValue + "</span></div></div></div>";
+                        counterLuckyNumbers++ ;
+                        $this.props.updateStatistics($value);
+                }, $ballwithBadgeInterval)
+                
+                counter++;             
+
+                setTimeout(function(){ 
+                        $this.Loop(true);}
+                , $clockMoveInterval);                       
+            }
+        }, $interval);
+    }
+}
+
+moveTopToBottom = (parentElement, startPosition) => {      
+      //var elem = document.getElementById(elementClass);   
+      let parentPos = parentElement.getBoundingClientRect(); 
+      let element = parentElement.children[0];
+      let pos = element.getBoundingClientRect();      
+      let id = setInterval(frame, 10);
+      let nextTop = pos.top;
+      let nextWidth = element.clientWidth;
+      let nextHeight = element.clientheigth;
+
+      function frame() {
+        if (nextTop == 350) {
+          clearInterval(id);
+        } else {
+          console.log('position of center ball : ' + nextTop.toString());          
+          nextTop++; 
+          nextHeight--;
+          nextWidth--;
+          element.style.marginTop = nextTop.toString() + 'px'; 
+          element.style.width = nextWidth.toString() + 'px'; 
+          element.style.height = nextHeight.toString() + 'px'; 
+          //elem.style.left = pos + 'px'; 
+        }
+      }
+}
+
+
+handleBallColor = ($value) => {
+    let color = "black";
+                
+    if($value >= 1 && $value < 10)
+        color = "yellow";
+    if($value >= 10 && $value < 20)
+        color = "red";
+    if($value >= 20 && $value < 30)
+        color = "blue";
+    if($value >= 30 && $value < 40)
+        color = "green";
+    
+    return color;
+}
+
+handleChange = (e) => {
+    console.log("div changed to "+  e.target.value);
+    alert('changed');
+}
+
+Loop = (move) => {
+    let ballSelector = document.getElementsByClassName('ball-circle');
+    let x, y;
+    for(var i = 0; i < ballSelector.length; i++) {
+        let circle = ballSelector[i];
+        let mainState = this;
+        let currentLeft = parseFloat(circle.style.left, 10);
+        let currentTop = parseFloat(circle.style.top, 10); 
+        const prevAngle = this.state.angles[i] - this.state.spc;
+        const currenctAngle = this.state.angles[i];
+        //console.log(this.state.angles[i]);
+
+        this.setState((prevState, props) => ({
+          angle: mainState.state.angles[i]
+        }));
+        //console.log(this.state.radiusSat);
+        x = this.state.cx + this.state.radius * Math.cos(this.state.angle * this.state.deg2rad);
+        y = this.state.cy + this.state.radius * Math.sin(this.state.angle * this.state.deg2rad);
+        
+        let nextLeft = x - this.state.radiusSat;
+        let nextTop = y - this.state.radiusSat;
+        
+        let newAngles = this.state.angles.slice() //copy the array
+        newAngles[i] = this.state.angles[i] + 18;//this.state.spc; //execute the manipulations
+        
+        this.setState((prevState, props) => ({
+          angles: newAngles
+        }));
+        
+        //console.log("this.state.angle: " + this.state.angle);
+        if(move)
+        {
+            let moveSlowly = setInterval(function(){    
+                   prevAngle++;                  
+
+                   let x = mainState.state.cx + mainState.state.radius * Math.cos(prevAngle * mainState.state.deg2rad);
+                   let y = mainState.state.cy + mainState.state.radius * Math.sin(prevAngle * mainState.state.deg2rad);
+        
+                   let nextLeft = x - mainState.state.radiusSat;
+                   let nextTop = y - mainState.state.radiusSat;
+                   //console.log("prevAngle: " + prevAngle + "currenctAngle: " + currenctAngle);
+                   //--- when movement ends, stop interval
+                   if(prevAngle == currenctAngle){
+                        clearInterval(moveSlowly);
+                   }
+                   else
+                   {
+                       circle.style.left = (nextLeft).toString() + "px";
+                       circle.style.top = (nextTop).toString() + "px";
+                   }
+            }, 5);
+        }
+        else{
+            circle.style.left = (nextLeft).toString() + "px";
+            circle.style.top = (nextTop).toString()+ "px";
+        }        
+    }
+}
+
+Animation = () => {
+    //console.log(document.getElementById('center'));
+
+    var centerElement = document.getElementsByClassName('center-circle')[0],
+    pos = centerElement.getBoundingClientRect(),
+    posOutsideCircle = document.getElementsByClassName('center-circle-outside')[0];
+    radiusSatByElement = document.getElementsByClassName('ball-circle')[0].offsetWidth * 0.5,
+    radiusByElement = centerElement.offsetWidth * 0.5,
+    cxByElement = pos.left + radiusByElement,
+    cyByElement = pos.top + radiusByElement,
+    $this = this,
+    numberOfBall = this.state.listItems.length,
+    spc = 360 / numberOfBall,
+    deg2radByElement = Math.PI / 180,
+    i = 0;
+    
+    posOutsideCircle.style.top = (pos.top - 88).toString() + "px";
+    posOutsideCircle.style.left = (pos.left - 88).toString() + "px";
+    posOutsideCircle.style.width = (centerElement.width + 100).toString() + "px";
+    posOutsideCircle.style.height = (centerElement.height + 100).toString() + "px";
+
+
+    this.setState({
+        radiusSat:radiusSatByElement,
+        radius:radiusByElement,
+        cx:cxByElement,
+        cy:cyByElement,
+        deg2rad:deg2radByElement,
+        spc:spc,
+        }, () => {
+    });    
+
+    let newAngles = this.state.angles.slice();// copy the array
+    let newAngle = 90;
+
+    for(let i = 0;i < numberOfBall; i++) {
+        newAngles.push(newAngle);
+        newAngle = newAngle - spc;
+        this.setState({angle: newAngle}, () => {            
+        }); 
+    }
+       
+   this.setState({angles:newAngles}, () => {
+    }); 
+
+   //console.log($this.state.angles);
+    
+    /// space out radius
+    radiusByElement += (radiusSatByElement + 10);
+    this.setState({,
+        radius:radiusByElement
+        }, () => {
+    });   
+
+    initialLoop = setInterval(function() {
+        $this.Loop(false);
+        clearInterval(initialLoop);
+    }, 1000);
+}
+
+render() {
+    let id = this.props.id;
+    let idPrefix =  this.props.id;
+    const className = this.props.className;
+    const badge = this.state.dataBadge;
+    const numberValue = this.state.numberValue;
+    let animation = this.props.animation;
+    if(this.props.list.length === 0)
+    {
+        return <h1>There are no list items</h1>;
+    }
+    else{        
+        return ( 
+                   
+            <div>                               
+                {this.state.listItems.map(function(listValue, index){ 
+                    id = idPrefix + "-" + index;  
+                    let inputId = "input-" + idPrefix + "-" + index;  
+                    let inputClassName = "input-" + className;
+
+                    return <div key={index} className={className}  data-badge={badge} id={id}>
+                                <LuckyNumber animation={animation} inputClassName = {inputClassName} luckyNumberValue={listValue} />                                                          
+                           </div>;
+                  })}
+            </div>
+        )  
+    }
+}
+}
+
+module.exports = Circle;
